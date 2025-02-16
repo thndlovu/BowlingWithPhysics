740de2d1c1ea9414c54d19c06c7ef6cbb6ea0bf9
thndlovu <tndlovu@student.ubc.ca>
2025-02-15 23:39:43 -0800

    Created logic for the ball object to move with the player object.
    Created BallAnchor object that allows the ball to move with the player without interfering with the firing logic of the ball object.

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,19 +6,23 @@ public class BallController : MonoBehaviour{
     private bool isBallLaunched;
     private Rigidbody ballRB;
     [SerializeField] private InputManager inputManager;
+    [SerializeField] private Transform ballAnchor;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start(){
         ballRB = GetComponent<Rigidbody>();
-
         inputManager.OnSpacePressed.AddListener(LaunchBall);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
     
     }
 
     // Update is called once per frame
     private void LaunchBall(){
         if(isBallLaunched) return;
-
         isBallLaunched = true;
+        transform.parent = null;
+        ballRB.isKinematic = false;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
