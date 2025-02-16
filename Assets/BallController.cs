fb1213bbc8a3b1bc867a51235e99d3aee08c9b00
thndlovu <tndlovu@student.ubc.ca>
2025-02-15 18:30:16 -0800

    Added BallController and InputManager scripts along with 9 bowling pin objects.
    Created the InputManager.cs that handles all of the inputs through UnityEvents. In addition I created the BallController.cs that is responsible for reacting to input manager and control the ball. The 9 pin objects were duplicated and arrange in a typical bowling format.

---
+++ Assets/BallController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BallController : MonoBehaviour{
+    [SerializeField] private float force = 1f;
+    private bool isBallLaunched;
+    private Rigidbody ballRB;
+    private InputManager inputManager;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start(){
+        ballRB = GetComponent<Rigidbody>();
+
+        inputManager.OnSpacePressed.AddListener(LaunchBall);
+    
+    }
+
+    // Update is called once per frame
+    private void LaunchBall(){
+        if(isBallLaunched) return;
+
+        isBallLaunched = true;
+        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+    }
+}
