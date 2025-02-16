de5ea30dac3aa5b6a4321721702bc8e332fef3ad
thndlovu <tndlovu@student.ubc.ca>
2025-02-15 23:11:18 -0800

    Player and Ball object Movement.
    Created Player.cs which handles the player's movement. Additionally added 2 physics materials (rough for the player and smooth for the ball).

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,10 +5,22 @@ using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour{
     public UnityEvent OnSpacePressed = new UnityEvent();
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
     // Update is called once per frame
     void Update(){
         if(Input.GetKeyDown(KeyCode.Space)){
             OnSpacePressed?.Invoke();
         }
+
+        Vector2 input = Vector2.zero;
+        if(Input.GetKey(KeyCode.A)){
+            input += Vector2.left;
+        }
+
+        if(Input.GetKey(KeyCode.D)){
+            input += Vector2.right;
+        }
+        
+        OnMove?.Invoke(input);
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour{
+
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private float speed;
+    private Rigidbody rb;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start(){
+        inputManager.OnMove.AddListener(MovePlayer);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void MovePlayer(Vector2 direction){
+        Vector3 moveDirection = new(direction.x, 0f, direction.y);
+        rb.AddForce(speed * moveDirection);
+    }
+}
